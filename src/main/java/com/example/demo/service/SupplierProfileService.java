a098242f6661f630ce2dcac6dac231ce4d044a5b
bala-amypo <bala@amypo.in>
2025-12-19 15:20:20 +0000

    User commit

--- src/main/java/com/example/demo/service/SupplierProfileService.java
+++ src/main/java/com/example/demo/service/SupplierProfileService.java
@@ -1,33 +1,12 @@
 package com.example.demo.service;
 
-import com.example.demo.model.SupplierProfile;
-import com.example.demo.repository.SupplierProfileRepository;
-import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
+import com.example.demo.model.SupplierProfile;
 
-@Service
-public class SupplierProfileService {
+public interface SupplierProfileService {
 
-    private final SupplierProfileRepository repository;
+    SupplierProfile createSupplier(SupplierProfile supplier);
 
-    public SupplierProfileService(SupplierProfileRepository repository) {
-        this.repository = repository;
-    }
-
-    public List<SupplierProfile> getAll() {
-        return repository.findAll();
-    }
-
-    public Optional<SupplierProfile> getById(Long id) {
-        return repository.findById(id);
-    }
-
-    public SupplierProfile save(SupplierProfile profile) {
-        return repository.save(profile);
-    }
-
-    public void delete(Long id) {
-        repository.deleteById(id);
-    }
+    List<SupplierProfile> getAllSuppliers();
 }
+
