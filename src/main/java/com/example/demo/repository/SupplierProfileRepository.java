720755efc61d58b2360813bbbcc6dec58c2e9c6e
bala-amypo <bala@amypo.in>
2025-12-26 08:59:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/SupplierProfileRepository.java
+++ src/main/java/com/example/demo/repository/SupplierProfileRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.SupplierProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-@Repository
-public interface SupplierProfileRepository
-        extends JpaRepository<SupplierProfile, Long> {
+public interface SupplierProfileRepository extends JpaRepository<SupplierProfile, Long> {
+
+    // Spring Data JPA automatically implements this method
+    Optional<SupplierProfile> findBySupplierCode(String supplierCode);
 }
