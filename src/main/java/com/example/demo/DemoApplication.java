9507b783750b8c7e382fe1c796f43fb1313fd6a4
bala-amypo <bala@amypo.in>
2025-12-26 19:13:32 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class DemonewApplication {
+@EnableJpaRepositories(basePackages = "com.example.demo.repository")
+public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemonewApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
