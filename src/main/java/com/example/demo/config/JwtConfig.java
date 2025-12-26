f3e96325f953fa1a9e50fb937478f2e79259c92a
bala-amypo <bala@amypo.in>
2025-12-26 16:45:16 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class JwtConfig {
-    // Placeholder config to avoid missing config errors.
-}
