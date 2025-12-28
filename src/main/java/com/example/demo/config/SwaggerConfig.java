05cdc44537c7aa97d8af139eea1656cf306f231f
bala-amypo <bala@amypo.in>
2025-12-28 11:15:55 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,7 +21,8 @@ public class SwaggerConfig {
                         .version("1.0")
                         .description("JWT-secured Supply Chain Analytics API")
                 )
-                // 🔐 Enable Authorize button
+                // 👇 IMPORTANT: define HTTPS server
+                .addServersItem(new Server().url("https://9034.pro604cr.amypo.ai"))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(
                         new io.swagger.v3.oas.models.Components()
